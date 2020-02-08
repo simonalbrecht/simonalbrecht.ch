3d506d181e55de1e8b7b88d961b17276c99bd2eb
Simon Albrecht <simon@simonalbrecht.ch>
2020-02-08 23:20:12 +0100

    Use master branch for Netlify CMS, linting

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,9 +1,7 @@
 <template>
-    <div class="">
-        <div class="container mx-auto min-w-full min-h-full w-screen h-screen max-w-full max-h-full flex flex-col justify-center">
-            <div class="container md:max-w-4xl px-4 sm:px-12">
-                <slot/>
-            </div>
+    <div class="container mx-auto min-w-full min-h-full w-screen h-screen max-w-full max-h-full flex flex-col justify-center">
+        <div class="container md:max-w-4xl px-4 sm:px-12">
+            <slot/>
         </div>
     </div>
 </template>
