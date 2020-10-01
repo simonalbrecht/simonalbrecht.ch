f98b4a5877eab7d487cd6d5b6a8865db460e1ff9
Simon Albrecht <simon@simonalbrecht.ch>
2020-10-01 18:12:31 +0200

    Fix title getting cut off on long content

--- src/components/StandaloneContent.vue
+++ src/components/StandaloneContent.vue
@@ -1,13 +1,13 @@
 <template>
   <Layout>
-    <header class="flex-auto mb-4" v-if="page.intro">
+    <header class="flex-auto" v-if="page.intro">
       <h1 class="font-bold text-3xl md:text-6xl" v-html="page.intro" />
     </header>
     <hr class="mb-8 md:mb-8 border-2 md:border-2 w-2/6 md:w-1/4 border-accent" />
 
     <section class="flex-auto mb-8 leading-normal font-light" v-html="page.content" />
 
-    <footer class="flex flex-row">
+    <footer class="flex flex-row mb-4">
       <a
         class="flex-initial mr-4 text-white hover:text-accent border-0 hover:border-0 opacity-75 hover:opacity-100"
         v-for="link in page.links"
