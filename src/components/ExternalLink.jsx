a01f2a6c38d72230b5718e4ea96d29e7e7129863
Simon Albrecht <simon@simonalbrecht.ch>
2020-12-29 15:01:57 +0100

    Remove title prop on ExternalLink altogether

--- src/components/ExternalLink.jsx
+++ src/components/ExternalLink.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const ExternalLink = ({ title, url, children }) => {
+const ExternalLink = ({ url, children }) => {
   return (
     <a
       className="flex-initial mr-4 text-text-dark dark:text-text-light hover:text-accent-dark dark:hover:text-accent-light hover: border-0 hover:border-0 opacity-75 hover:opacity-100"
       href={url}
-      title={title}
       target="_blank"
       rel="nofollow, noopener, noreferrer"
     >
@@ -16,7 +15,6 @@ const ExternalLink = ({ title, url, children }) => {
 };
 
 ExternalLink.propTypes = {
-  title: PropTypes.string,
   url: PropTypes.string.isRequired,
   children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]),
 };
